e042ffe3853f87d0c8cc0beaf8b50324ddc4959e
minjikim <inkmjj@gmail.com>
2022-09-23 22:12:06 +0900

    입출력과 사칙연산 - 개

---
+++ minji/week1/IO&Arithmetic/no25083.cpp
@@ -0,0 +1,15 @@
+//
+// Created by 김민지 on 2022/09/23.
+//
+
+#include "iostream"
+using namespace std;
+
+int main(void) {
+    cout << "         ,r'\"7" << endl;
+    cout << "r`-_   ,'  ,/" << endl;
+    cout << " \\. \". L_r'" << endl;
+    cout << "   `~\\/" << endl;
+    cout << "      |" << endl;
+    cout << "      |" << endl;
+}
