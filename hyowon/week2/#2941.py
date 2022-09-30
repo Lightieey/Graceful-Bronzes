21598690221f7a6687aebe1678ba7adcec155aaa
이효원 <ymj07168@gmail.com>
2022-09-30 19:35:05 +0900

    2주차 끝

--- hyowon/week2/#2941.py
+++ hyowon/week2/#2941.py
@@ -2,7 +2,8 @@
 
 # 문자열에서 특정문자열 제거방법
 # strip(regex) replace(regex, ''), sub(regex, '', str)
-
+# strip은 양 끝단의 문자열만 삭제할 수 있다.
+# count 문자열에서 특정 문자열 개수 세는 데 사용
 import sys
 
 word = sys.stdin.readline()
@@ -14,18 +15,17 @@ croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 alphabet = []
 for c in croatia:
     if c in word:
-        alphabet.append(c)
-        word = word.strip(c)
-        # new_word = word.strip(c)
-        # word, new_word = new_word, word
-        print(word)
+        for i in range(word.count(c)):
+            alphabet.append(c)
+        word = word.replace(c, ' ')
 
 
 # 나머지 알파벳 분리
 for w in word:
-    alphabet.append(w)
+    if w != '\n' and w != ' ':
+        alphabet.append(w)
 
-print(alphabet)
+print(len(alphabet))
 
 
 
