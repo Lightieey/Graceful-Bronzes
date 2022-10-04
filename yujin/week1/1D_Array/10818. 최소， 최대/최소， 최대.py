a25ab842d8756a31679aebba8cab3907d7ba78de
이효원 <ymj07168@gmail.com>
2022-10-04 18:55:44 +0900

    3주차 Math1 폴더

--- yujin/week1/1D_Array/10818. 최소， 최대/최소， 최대.py
+++
@@ -1,4 +0,0 @@
-import sys
-n = int(sys.stdin.readline())
-n_list = list(map(int, input().split()))
-print(min(n_list), max(n_list))
--- yujin/week1/Loop/2439. 별 찍기 － 2/별 찍기 － 2.py
+++
@@ -1,9 +0,0 @@
-import sys
-n = int(sys.stdin.readline())
-for i in range(0, n):
-  for j in range(0, n):
-    if j < (n-i-1):
-      print(" ", end='')
-    else:
-      print("*", end='')
-  print()
