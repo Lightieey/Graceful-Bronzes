5513b599ff03b6a3d1a6c7ecd1c6729a24b8f5eb
이효원 <ymj07168@gmail.com>
2022-10-08 02:59:34 +0900

    3주차 #베르트랑공준 시간초과 구글링

--- hyowon/week3/Math2/#4948.py
+++ hyowon/week3/Math2/#4948.py
@@ -1,5 +1,18 @@
 # 베르트랑 공준
 import sys
+import math
+
+def isPrime(num):
+    if num == 1:
+        return False
+    elif num == 2:
+        return True
+
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            return False
+    return True
+
 
 case_List = []
 while True:
@@ -12,21 +25,43 @@ count_List = []
 for case in case_List:
     count = 0
     for c in range(case+1, 2*case+1):
-        flag = 1
-        if c == 1:
-            flag = 0
-        # 2를 제외한 짝수
-        elif c % 2 == 0 and c != 2:
-            flag = 0
-        else:
-            for i in range(2, c):
-                if c % i == 0:
-                    flag = 0
-                    break
-        if flag == 1:
+        if c%2 == 0 and c != 2:
+            continue
+        elif isPrime(c):
             count += 1
     count_List.append(count)
 
 for count in count_List:
     print(count)
 
+
+
+## 구글링 코드
+
+# def is_prime(n):
+#     if n == 2:
+#         return True
+#
+#     if n % 2 == 0:
+#         return False
+#
+#     for i in range(2, int(math.sqrt(n)) + 1):
+#         if n % i == 0:
+#             return False
+#
+#     return True
+#
+#
+# while True:
+#     n = int(sys.stdin.readline())
+#
+#     if n == 0:
+#         break
+#
+#     prime_cnt = 0
+#
+#     for i in range(n + 1, (2 * n) + 1):
+#         if is_prime(i):
+#             prime_cnt += 1
+#
+#     print(prime_cnt)
