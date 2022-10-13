89f52a023792b938c7a439e704dcf1d5f6057259
minjikim <inkmjj@gmail.com>
2022-10-13 20:36:10 +0900

    수 정렬하기 - 선택정렬

--- minji/week4/no2750.cpp
+++ minji/week4/no2750.cpp
@@ -2,7 +2,6 @@
 // Created by 김민지 on 2022/10/10.
 //
 #include "iostream"
-#include "algorithm"
 using namespace std;
 
 int main() {
@@ -12,14 +11,21 @@ int main() {
     int nums[n];
 
     for (int i = 0; i < n; i++) {
-        int num;
-        cin >> num;
-        nums[i] = num;
+        cin >> nums[i];
     }
 
-    sort(nums, nums + n);
-
-    for (int i = 0; i < n; i++) {
-        cout << nums[i] << "\n";
+    for (int i = 0; i < n-1; i++) {
+        for (int j = i+1; j < n; j++) {
+            if (nums[j] < nums[i]) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+            }
+        }
     }
+
+    for (int num : nums) {
+        cout << num << "\n";
+    }
+
 }
