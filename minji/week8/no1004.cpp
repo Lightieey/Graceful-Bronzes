73a28117c33200df5d2632aaccf79ec474747dd7
minjikim <inkmjj@gmail.com>
2022-11-19 09:48:28 +0900

    기하1 - 어린왕자

--- minji/week8/no1004.cpp
+++ minji/week8/no1004.cpp
@@ -24,11 +24,6 @@ int main() {
                 if ((x2-cx)*(x2-cx)+(y2-cy)*(y2-cy) < r*r)
                     cnt++;
 
-            /*bool cond1 = (cx-r < x1 && x1 < cx+r) && (cy-r < y1 && y1 < cy+r);
-            bool cond2 = (cx-r < x2 && x2 < cx+r) && (cy-r < y2 && y2 < cy+r);
-            if (cond1 && cond2) continue;
-            if (cond1) cnt++;
-            if (cond2) cnt++;*/
         }
         cout << cnt << "\n";
     }
